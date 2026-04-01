c1568f34cea54e456751797ebb4427926061b4da
Lanzin Code <lanzincode@gmail.com>
2026-04-01 14:17:20 -0300

    primeiro commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  
+</script>
+
+<main>
+  <section class="card">
+    <img src="src/assets/image-qr-code.png" alt="QR Code" />
+    <h1>Improve your front-end skills by building projects</h1>
+    <p>
+      Scan the QR code to visit Frontend Mentor and take your coding skills to
+      the next level
+    </p>
+  </section>
+</main>
